3de1378d7de77ee434ef40aefd10e1f4197723e7
laportenier <lilly.portenier@gmail.com>
2019-12-19 19:41:04 -0500

    added .gitignore file and .env file so each team member can access the database on there computer

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+module.exports = {
+
+  "development": {
+    "username": "root",
+      "password": process.env.LOCAL_PASSWORD,
+        "database": "user_db",
+          "host": "localhost",
+            "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+      "password": null,
+        "database": "testdb",
+          "host": "localhost",
+            "dialect": "mysql",
+              "logging": false
+  },
+  "production": {
+    "use_env_variable": "JAWSDB_URL",
+      "dialect": "mysql"
+  }
+
+}
