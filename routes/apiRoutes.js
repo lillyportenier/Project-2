33bb57b7b318c054432b1a21b81a63d1d7c7eab4
laportenier <lilly.portenier@gmail.com>
2020-01-04 12:10:04 -0500

    commit to pull

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -84,18 +84,18 @@ module.exports = function(app) {
 //   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
 //   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
 //   // otherwise send back an error
-//   app.post("/api/signup", function(req, res) {
-//     db.User.create({
-//       email: req.body.email,
-//       password: req.body.password
-//     })
-//       .then(function() {
-//         res.redirect(307, "/api/login");
-//       })
-//       .catch(function(err) {
-//         res.status(401).json(err);
-//       });
-//   });
+  // app.post("/api/signup", function(req, res) {
+  //   db.User.create({
+  //     email: req.body.email,
+  //     password: req.body.password
+  //   })
+  //     .then(function() {
+  //       res.redirect(307, "/api/login");
+  //     })
+  //     .catch(function(err) {
+  //       res.status(401).json(err);
+  //     });
+  // });
 
 //   // Route for logging user out
 //   app.get("/logout", function(req, res) {
