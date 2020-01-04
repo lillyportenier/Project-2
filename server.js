6c2b1d0e67576bb2b76fae1224f61ba35309c97a
Gabriel Mixon <gabrielmixon@gmail.com>
2020-01-04 10:56:04 -0500

    got the keyboard cat thing working

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
+var session = require("express-session");
 var passport = require("./config/passport");
 
 var db = require("./models");
