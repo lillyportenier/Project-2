79861cb287ce8316b6ed5a67b3d19eaf0afbe194
Gabriel Mixon <gabrielmixon@gmail.com>
2020-01-05 11:50:56 -0500

    seeing if any of my changes are different from master

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+var passport = require("../config/passport");
 
 module.exports = function(app) {
   // Get all Posts
@@ -24,15 +25,15 @@ module.exports = function(app) {
   
   
 // var db = require("../models");
-// var passport = require("../config/passport");
+
 
 // module.exports = function(app) {
   //   // Using the passport.authenticate middleware with our local strategy.
   //   // If the user has valid login credentials, send them to the members page.
   //   // Otherwise the user will be sent an error
-  //   app.post("/api/login", passport.authenticate("local"), function(req, res) {
-    //     res.json(req.user);
-    //   });
+    app.post("/api/login", passport.authenticate("local"), function(req, res) {
+        res.json(req.user);
+      });
     
     //   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
     //   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
